ffc929eab3591515d01846fa029f4039db27e878
junderw <junderwood@bitcoinbank.co.jp>
2020-04-25 09:24:36 +0900

    Refactor tests a little

--- ts_src/index.spec.ts
+++ ts_src/index.spec.ts
@@ -14,8 +14,8 @@ const APIPASS = process.env['APIPASS'] || 'satoshi';
 let regtestUtils: RegtestUtils;
 
 const HOST = process.env['BTCPAY_HOST'] || 'http://127.0.0.1:49392';
-const KP1 = btcPayCrypto.generate_keypair();
-const KP2 = btcPayCrypto.generate_keypair();
+let KP1: any;
+let KP2: any;
 let btcPayClientSegwit: BTCPayClient;
 let btcPayClientSegwitP2SH: BTCPayClient;
 
@@ -26,6 +26,9 @@ describe('requestPayjoin', () => {
   beforeAll(async () => {
     jest.setTimeout(20000);
     regtestUtils = new RegtestUtils({ APIURL, APIPASS });
+
+    KP1 = btcPayCrypto.generate_keypair();
+    KP2 = btcPayCrypto.generate_keypair();
 
     const browser = await getBrowser();
 
@@ -55,114 +58,83 @@ describe('requestPayjoin', () => {
     expect(typeof requestPayjoinWithCustomRemoteCall).toBe('function');
   });
   it('should request p2sh-p2wpkh payjoin', async () => {
-    const invoice = await btcPayClientSegwitP2SH.create_invoice({
-      currency: 'USD',
-      price: 1.12,
-    });
-    const pjEndpoint = qs.decode(invoice.paymentUrls.BIP21 as string)
-      .pj as string;
-
-    const keyPair = bitcoin.ECPair.makeRandom({ network });
-    const p2sh = bitcoin.payments.p2sh({
-      redeem: bitcoin.payments.p2wpkh({
-        pubkey: keyPair.publicKey,
-        network,
-      }),
-      network,
-    });
-    const unspent = await regtestUtils.faucet(p2sh.address!, 2e7);
-    const sendAmount = Math.round(parseFloat(invoice.btcPrice) * 1e8);
-    const psbt = new bitcoin.Psbt({ network })
-      .addInput({
-        hash: unspent.txId,
-        index: unspent.vout,
-        redeemScript: p2sh.redeem!.output!,
-        witnessUtxo: {
-          script: p2sh.output!,
-          value: unspent.value,
-        },
-      })
-      .addOutput({
-        address: invoice.bitcoinAddress,
-        value: sendAmount,
-      })
-      .addOutput({
-        address: p2sh.address!,
-        value: unspent.value - sendAmount - 10000,
-      })
-      .signInput(0, keyPair);
-    const newPsbt = await requestPayjoin(psbt, pjEndpoint);
-    newPsbt.data.inputs.forEach((psbtInput, i) => {
-      if (
-        psbtInput.finalScriptSig === undefined &&
-        psbtInput.finalScriptWitness === undefined
-      ) {
-        newPsbt.signInput(i, keyPair).finalizeInput(i);
-      }
-    });
-    const tx = newPsbt.extractTransaction();
-    await regtestUtils.broadcast(tx.toHex());
-    await regtestUtils.verify({
-      txId: tx.getId(),
-      address: bitcoin.address.fromOutputScript(tx.outs[1].script, network),
-      vout: 1,
-      value: tx.outs[1].value,
-    });
-    expect(tx).toBeDefined();
+    await testPayjoin(btcPayClientSegwitP2SH, getP2SHP2WPKH);
   });
   it('should request p2wpkh payjoin', async () => {
-    const invoice = await btcPayClientSegwit.create_invoice({
-      currency: 'USD',
-      price: 1.12,
-    });
-    const pjEndpoint = qs.decode(invoice.paymentUrls.BIP21 as string)
-      .pj as string;
-
-    const keyPair = bitcoin.ECPair.makeRandom({ network });
-    const p2wpkh = bitcoin.payments.p2wpkh({
-      pubkey: keyPair.publicKey,
-      network,
-    });
-    const unspent = await regtestUtils.faucet(p2wpkh.address!, 2e7);
-    const sendAmount = Math.round(parseFloat(invoice.btcPrice) * 1e8);
-    const psbt = new bitcoin.Psbt({ network })
-      .addInput({
-        hash: unspent.txId,
-        index: unspent.vout,
-        witnessUtxo: {
-          script: p2wpkh.output!,
-          value: unspent.value,
-        },
-      })
-      .addOutput({
-        address: invoice.bitcoinAddress,
-        value: sendAmount,
-      })
-      .addOutput({
-        address: p2wpkh.address!,
-        value: unspent.value - sendAmount - 10000,
-      })
-      .signInput(0, keyPair);
-    const newPsbt = await requestPayjoin(psbt, pjEndpoint);
-    newPsbt.data.inputs.forEach((psbtInput, i) => {
-      if (
-        psbtInput.finalScriptSig === undefined &&
-        psbtInput.finalScriptWitness === undefined
-      ) {
-        newPsbt.signInput(i, keyPair).finalizeInput(i);
-      }
-    });
-    const tx = newPsbt.extractTransaction();
-    await regtestUtils.broadcast(tx.toHex());
-    await regtestUtils.verify({
-      txId: tx.getId(),
-      address: bitcoin.address.fromOutputScript(tx.outs[1].script, network),
-      vout: 1,
-      value: tx.outs[1].value,
-    });
-    expect(tx).toBeDefined();
+    await testPayjoin(btcPayClientSegwit, getP2WPKH);
   });
 });
+
+async function testPayjoin(
+  btcPayClient: BTCPayClient,
+  getPayment: (pubkey: Buffer) => bitcoin.Payment,
+): Promise<void> {
+  const invoice = await btcPayClient.create_invoice({
+    currency: 'USD',
+    price: 1.12,
+  });
+  const pjEndpoint = qs.decode(invoice.paymentUrls.BIP21 as string)
+    .pj as string;
+
+  const keyPair = bitcoin.ECPair.makeRandom({ network });
+  const payment = getPayment(keyPair.publicKey);
+  const unspent = await regtestUtils.faucet(payment.address!, 2e7);
+  const sendAmount = Math.round(parseFloat(invoice.btcPrice) * 1e8);
+  const psbt = new bitcoin.Psbt({ network })
+    .addInput({
+      hash: unspent.txId,
+      index: unspent.vout,
+      witnessUtxo: {
+        script: payment.output!,
+        value: unspent.value,
+      },
+      ...(payment.redeem ? { redeemScript: payment.redeem.output! } : {}),
+    })
+    .addOutput({
+      address: invoice.bitcoinAddress,
+      value: sendAmount,
+    })
+    .addOutput({
+      address: payment.address!,
+      value: unspent.value - sendAmount - 10000,
+    })
+    .signInput(0, keyPair);
+  const newPsbt = await requestPayjoin(psbt, pjEndpoint);
+  newPsbt.data.inputs.forEach((psbtInput, i) => {
+    if (
+      psbtInput.finalScriptSig === undefined &&
+      psbtInput.finalScriptWitness === undefined
+    ) {
+      newPsbt.signInput(i, keyPair).finalizeInput(i);
+    }
+  });
+  const tx = newPsbt.extractTransaction();
+  await regtestUtils.broadcast(tx.toHex());
+  await regtestUtils.verify({
+    txId: tx.getId(),
+    address: bitcoin.address.fromOutputScript(tx.outs[1].script, network),
+    vout: 1,
+    value: tx.outs[1].value,
+  });
+  expect(tx).toBeDefined();
+}
+
+function getP2WPKH(pubkey: Buffer) {
+  return bitcoin.payments.p2wpkh({
+    pubkey,
+    network,
+  });
+}
+
+function getP2SHP2WPKH(pubkey: Buffer) {
+  return bitcoin.payments.p2sh({
+    redeem: bitcoin.payments.p2wpkh({
+      pubkey,
+      network,
+    }),
+    network,
+  });
+}
 
 // Used for getting a new pairing code
 const HEADLESS = true;
